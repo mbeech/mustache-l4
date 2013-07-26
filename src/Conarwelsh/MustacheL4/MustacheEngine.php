fc7e365bd5c23c4bca3208dccaf408d8789307c5
Conar Welsh <conar@welshlabs.com>
2013-07-26 08:17:02 -0600

    added is_object check
    added a check to ensure that the $item was an object before checking if the method exists

--- src/Conarwelsh/MustacheL4/MustacheEngine.php
+++ src/Conarwelsh/MustacheL4/MustacheEngine.php
@@ -19,7 +19,7 @@ class MustacheEngine implements EngineInterface {
 		$m = new Mustache_Engine( $app['config']->get('mustache-l4::config') );
  
  		$data = array_map(function($item){
-			return method_exists($item, 'toArray') ? $item->toArray() : $item;
+			return (is_object($item) && method_exists($item, 'toArray')) ? $item->toArray() : $item;
 		}, $data);
  
 		return $m->render($view, $data);
